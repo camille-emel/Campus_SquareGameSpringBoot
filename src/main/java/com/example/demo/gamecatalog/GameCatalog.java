fc6ed49905715bfbe04f77c9138ec4eab6a8f8e5
Camille EMEL <camille.emel@le-campus-numerique.fr>
2024-12-19 16:13:37 +0100

    bonne vac

--- src/main/java/com/example/demo/gamecatalog/GameCatalog.java
+++ src/main/java/com/example/demo/gamecatalog/GameCatalog.java
@@ -1,10 +1,11 @@
 package com.example.demo.gamecatalog;
 
+import com.example.demo.gamecatalog.plugin.GamePlugin;
 import fr.le_campus_numerique.square_games.engine.GameFactory;
 
 import java.util.Collection;
 //permettant de récupérer la liste des identiants des jeux d
 public interface GameCatalog {
-    Collection<String> getGamesIdentifiers();
+    Collection<GamePlugin> getPlugins();
     GameFactory getFactory(String Id);
 }
--- src/main/java/com/example/demo/gamecatalog/GameCatalogController.java
+++ src/main/java/com/example/demo/gamecatalog/GameCatalogController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collection;
+import java.util.Locale;
+import java.util.Map;
 
 @RestController
 public class GameCatalogController {
@@ -12,7 +14,12 @@ public class GameCatalogController {
     private GameCatalog gameCatalog;
 
     @GetMapping("/games/catalog")
-    public Collection<String> getGames() {
-       return gameCatalog.getGamesIdentifiers();
+    public Collection<?> getGames(Locale locale) {
+        return this.gameCatalog.getPlugins().stream()
+                .map(plugin-> Map.of(
+                        "id","",
+                        "name",plugin.getName(locale)
+                        ))
+                .toList();
     }
 }
